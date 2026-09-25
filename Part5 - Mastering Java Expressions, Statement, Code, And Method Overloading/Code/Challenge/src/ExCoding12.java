7c1e8dfb374fbdcc547d0fb9f95a43950a800ea4
sonnt119 <sonnt119@viettelsoftware.com>
2026-09-25 14:05:13 +0700

    Update Code 250926

--- Part5 - Mastering Java Expressions, Statement, Code, And Method Overloading/Code/Challenge/src/ExCoding12.java
+++ Part5 - Mastering Java Expressions, Statement, Code, And Method Overloading/Code/Challenge/src/ExCoding12.java
@@ -3,4 +3,20 @@ public class ExCoding12 {
     static void main(String[] args) {
 
     }
+
+    //    public static boolean isCatPlaying (boolean summer, int temperature) {
+    //        if (summer == true && (temperature >= 25 && temperature <= 45)) {
+    //            return true;
+    //        } else if ((summer == false && (temperature >= 25 && temperature <= 35))) {
+    //            return true;
+    //        }
+    //
+    //        return false;
+    //    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+        int max = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= max;
+    }
 }
